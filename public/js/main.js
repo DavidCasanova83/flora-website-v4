e32217e42470e93a9da5fdc1af577484ee873b2f
David Casanova <david.casanova.pro@gmail.com>
2025-04-08 21:19:20 +0200

    add texte and design update + json avis

--- public/js/main.js
+++ public/js/main.js
@@ -31,101 +31,101 @@ const swiper = new Swiper('.swiper', {
 const faqItems = document.querySelectorAll('.faq__itm');
 
 faqItems.forEach((item) => {
-   const faqBtn = item.querySelector('.faq__btn');
+  const faqBtn = item.querySelector('.faq__btn');
 
-   item.addEventListener('click', () => {
-      const isOpen = item.classList.toggle('open');
-      const iconClass = isOpen ? 'ri-substract-fill' : 'ri-add-fill';
-      const iconElement = faqBtn.querySelector('i');
-      iconElement.classList = `${iconClass} text-2xl`;
-   });
+  item.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('open');
+    const iconClass = isOpen ? 'ri-substract-fill' : 'ri-add-fill';
+    const iconElement = faqBtn.querySelector('i');
+    iconElement.classList = `${iconClass} text-2xl`;
+  });
 });
 
 
-//scroll reveal animations
-const sr = ScrollReveal({
-  origin: 'bottom',
-  distance: '60px',
-  duration : 3000,
-  delay: 400,
-  //reset: true // resets animation
-});
+// //scroll reveal animations
+// const sr = ScrollReveal({
+//   origin: 'bottom',
+//   distance: '60px',
+//   duration: 3000,
+//   delay: 400,
+//   //reset: true // resets animation
+// });
 
-//hero
-sr.reveal('.hero__text', { origin: 'top',delay: 50,  duration : 3000});
-sr.reveal('.hero__img');
+// //hero
+// sr.reveal('.hero__text', { origin: 'top', delay: 50, duration: 3000 });
+// sr.reveal('.hero__img');
 
 
 // //slider photo home
 // sr.reveal('.slider__photo');
 
 
-//stats
-sr.reveal('.stats__item', { delay: 600, distance: '100px', interval: 100, origin: 'top'});
+// //stats
+// sr.reveal('.stats__item', { delay: 600, distance: '100px', interval: 100, origin: 'top'});
 
-// services
-sr.reveal('.services');
-sr.reveal('.services__top');
-sr.reveal('.services__item', {
-  delay: 600,
-  distance :'100px',
-  interval:100,
-  origin:'top'
-});
-
-// appointment
-sr.reveal('.appointment__title', { origin: 'top',delay: 50,  duration : 3000});
-sr.reveal('.appointment__form');
-
-// testimonials
-sr.reveal('.testimonials');
-sr.reveal('.testimonials_container');
-
-// team
-sr.reveal('.team__title');
-sr.reveal('.team__slider');
-
-// faq
-sr.reveal('.faq__title');
-sr.reveal('.faq__item',{
-  delay: 600,
-  distance :'100px',
-  interval:100,
-  origin:'top'
-});
-
-// departments
-sr.reveal('.departments__bg');
-sr.reveal('.departments__container');
-
-
-// blog
-sr.reveal('.blog__title');
-sr.reveal('.blog__post',{
-  delay: 600,
-  distance :'100px',
-  interval:100,
-  origin:'top'
-});
-
-// brands
-sr.reveal('.brands__logo',{
-  delay: 600,
-  distance :'100px',
-  interval:100,
-  origin:'top'
-});
-
-
-// newsletter
-sr.reveal('.newsletter');
-sr.reveal('.newsletter__container');
-
-// // footer
-
-// sr.reveal('.footer__item',{
+// // services
+// sr.reveal('.services');
+// sr.reveal('.services__top');
+// sr.reveal('.services__item', {
 //   delay: 600,
 //   distance :'100px',
 //   interval:100,
 //   origin:'top'
 // });
+
+// // appointment
+// sr.reveal('.appointment__title', { origin: 'top',delay: 50,  duration : 3000});
+// sr.reveal('.appointment__form');
+
+// // testimonials
+// sr.reveal('.testimonials');
+// sr.reveal('.testimonials_container');
+
+// // team
+// sr.reveal('.team__title');
+// sr.reveal('.team__slider');
+
+// // faq
+// sr.reveal('.faq__title');
+// sr.reveal('.faq__item',{
+//   delay: 600,
+//   distance :'100px',
+//   interval:100,
+//   origin:'top'
+// });
+
+// // departments
+// sr.reveal('.departments__bg');
+// sr.reveal('.departments__container');
+
+
+// // blog
+// sr.reveal('.blog__title');
+// sr.reveal('.blog__post',{
+//   delay: 600,
+//   distance :'100px',
+//   interval:100,
+//   origin:'top'
+// });
+
+// // brands
+// sr.reveal('.brands__logo',{
+//   delay: 600,
+//   distance :'100px',
+//   interval:100,
+//   origin:'top'
+// });
+
+
+// // newsletter
+// sr.reveal('.newsletter');
+// sr.reveal('.newsletter__container');
+
+// // // footer
+
+// // sr.reveal('.footer__item',{
+// //   delay: 600,
+// //   distance :'100px',
+// //   interval:100,
+// //   origin:'top'
+// // });
